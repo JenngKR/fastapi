01f30dba0767740b5651a9be7006c3b5785bd42a
정준혁 <ghgjkl1234@naver.com>
2023-09-11 05:46:52 +0900

    파일 수정

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from domain.answer import answer_router
+from domain.question import question_router
+from domain.user import user_router
+
 app = FastAPI()
 
 origins = [
@@ -16,7 +20,6 @@ app.add_middleware(
 )
 
 
-@app.get("/hello")
-def hello():
-    return {"message": "안녕하세요 파이보"}
-
+app.include_router(question_router.router)
+app.include_router(answer_router.router)
+app.include_router(user_router.router)
